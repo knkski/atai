a2877645d2a6431f764a5628a3e2936bf36503ad
knkski <kennethkoski@gmail.com>
2017-09-25 11:55:55 -0500

    Working on visualization

---
+++ visualize.py
@@ -0,0 +1,48 @@
+from scipy.ndimage import imread
+import keras
+import keras.backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def main():
+    model = keras.models.load_model('model.h5')
+    image = imread(sys.argv[1]).reshape(1, 28, 28, 1)
+
+    activation_maps = [
+        (layer.name, K.function([model.input, K.learning_phase()], [layer.output])([image, 0.])[0])
+        for layer in model.layers
+    ]
+
+    for layer_name, activation_map in activation_maps:
+        print('Activation map for layer %s %s' % (layer_name, activation_map.shape))
+
+        if len(activation_map.shape) == 4:
+            # Convert from vertically-stacked images to side-by-side in a line
+            letters = np.hstack(np.transpose(activation_map[0], (2, 0, 1)))
+
+            # Rearrange those images into a square
+            activations = np.vstack(np.split(letters, int(activation_map.shape[-1] ** 0.5), 1))
+
+        elif len(activation_map.shape) == 2:
+            # try to make it square as much as possible. we can skip some activations.
+            activations = activation_map[0]
+            num_activations = len(activations)
+            if num_activations > 1024:  # too hard to display it on the screen.
+                square_param = int(np.floor(np.sqrt(num_activations)))
+                activations = activations[0: square_param * square_param]
+                activations = np.reshape(activations, (square_param, square_param))
+            else:
+                activations = np.expand_dims(activations, axis=0)
+
+        else:
+            raise Exception('Can\'t deal with shape %s' % (activation.shape,))
+
+        plt.imshow(activations, interpolation='None', cmap='binary_r')
+        plt.axis('off')
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
